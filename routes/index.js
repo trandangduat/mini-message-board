2ffef252289a250aa371ee9c1ee01c66ef489a79
Tran Dang Duat <23020023@vnu.edu.vn>
2024-05-12 01:14:46 +0700

    Create GET/POST request for /new

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hello",
+    user: "Duat",
+    added: new Date()
+  },
+  {
+    text: "Hello again",
+    user: "Kussso",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({
+    text: req.body.content,
+    user: req.body.username,
+    added: new Date()
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
