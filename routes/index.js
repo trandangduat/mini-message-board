4fc79c303a80f7a221e42e80f42b9a204a8e1b0f
Tran Dang Duat <23020023@vnu.edu.vn>
2024-05-12 13:57:13 +0700

    Style changes + Add New Message button in index route

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ dayjs.extend(relativeTime);
 
 const messages = [
   {
-    text: "Hello",
-    user: "Duat",
+    text: "Me when the imposus sussy baka bussy is not sussy fr fr",
+    user: "ngoitrongtoilet_gaothettemem",
     added: new Date()
   },
   {
-    text: "Hello again",
-    user: "Kussso",
+    text: "level 5 gyatt rizz livvy dunne rizzing up baby gronk ice spice wat da dawg doin skibidi toilet in real life only in ohio we go jim zyzz creatine alpha sigma cuh dey board",
+    user: "sieunhangao@123",
     added: new Date()
   }
 ]
@@ -22,7 +22,7 @@ router.get('/', function(req, res, next) {
   const formattedMessages = messages.map((message) => {
     return {
       ...message,
-      added: dayjs(message.added).fromNow(true)
+      added: dayjs(message.added).fromNow()
     }
   });
   res.render('index', { title: 'Message Board', messages: formattedMessages });
